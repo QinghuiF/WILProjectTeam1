1b0ef08a8b885787557c1392c8f0577648795154
QinghuiF <qinghui0818@gmail.com>
2023-08-19 13:44:51 +0930

    asp.net template with registration and login functions

---
+++ WILTeam1/WILTeam1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WILTeam1.Startup))]
+namespace WILTeam1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
